2f20fe76cca0f586649b3cd532d5ba607c41194a
Chhoeng Sina <sinavlog2020@gmail.com>
2021-08-29 20:30:09 +0700

    --

---
+++ app.js
@@ -0,0 +1,18 @@
+const openNav = document.querySelector('.hamburger');
+const closeNav = document.querySelector('.nav_close');
+const nav = document.querySelector('.nav_list');
+openNav.addEventListener ("click", ()=>{
+    const navLeft = nav.getBoundingClientRect().left;
+    if (navLeft < 0){
+        nav.style.left=0;
+        document.body.classList.add('active');
+    }
+});
+
+closeNav.addEventListener("click", () => {
+    const navLeft = nav.getBoundingClientRect().left;
+    if (navLeft === 0) {
+      nav.style.left = "-40rem";
+      document.body.classList.remove("active");
+    }
+  });
